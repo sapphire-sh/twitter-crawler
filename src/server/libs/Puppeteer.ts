e0f00b4299bcc77d7203d29fe8979606c8e8a439
sapphire <info@sapphire.sh>
2018-11-08 10:52:18 +0900

    update parser

--- src/server/libs/Puppeteer.ts
+++ src/server/libs/Puppeteer.ts
@@ -7,6 +7,10 @@ import {
 } from './Processor';
 
 import {
+	GoogleSpreadsheets,
+} from '../libs';
+
+import {
 	Command,
 	Manifest,
 } from '../models';
@@ -43,6 +47,16 @@ export class Puppeteer extends Processor {
 		return this.instance;
 	}
 
+	private clearInput(page: puppeteer.Page, selector: string) {
+		return page.evaluate((selector: string) => {
+			const field = document.querySelector(selector) as HTMLInputElement;
+			if(field === null) {
+				return;
+			}
+			field.value = '';
+		}, selector);
+	}
+
 	public async initialize() {
 		const {
 			screen_name,
@@ -56,8 +70,40 @@ export class Puppeteer extends Processor {
 		await page.goto('https://twitter.com/login', {
 			'waitUntil': 'domcontentloaded',
 		});
-		await page.type('.js-username-field', screen_name);
-		await page.type('.js-password-field', password);
+
+		{
+			let shouldWait = true;
+			do {
+				await sleep(500);
+
+				await this.clearInput(page, '.js-username-field');
+				await page.type('.js-username-field', screen_name);
+
+				await sleep(500);
+
+				await this.clearInput(page, '.js-password-field');
+				await page.type('.js-password-field', password);
+
+				const values = await page.evaluate((_) => {
+					const usernameField = document.querySelector('.js-username-field') as HTMLInputElement;
+					const passwordField = document.querySelector('.js-password-field') as HTMLInputElement;
+
+					if(usernameField === null || passwordField === null) {
+						return null;
+					}
+					return {
+						'username': usernameField.value,
+						'password': passwordField.value,
+					};
+				});
+
+				if(values.username === screen_name && values.password === password) {
+					shouldWait = false;
+				}
+			}
+			while(shouldWait);
+		}
+
 		await page.keyboard.press('Enter');
 
 		let shouldWait = true;
@@ -85,6 +131,8 @@ export class Puppeteer extends Processor {
 			return;
 		}
 
+		const spreadsheets = GoogleSpreadsheets.getInstance();
+
 		await sleep(500);
 
 		const page = await this.browser.newPage();
@@ -105,42 +153,73 @@ export class Puppeteer extends Processor {
 		}
 		while(shouldWait);
 
+		let tweets: Array<{
+			link: string | null;
+			images: string[];
+		}> = [];
+		let prevLength = -1;
 		let shouldScroll = true;
+		let count = 0;
 		do {
+			prevLength = tweets.length;
+
 			await sleep(500);
+
+			await page.evaluate((_) => {
+				window.scrollBy(0, document.documentElement!.scrollHeight);
+			});
+
+			tweets = await page.evaluate((_) => {
+				return Array.from(document.querySelectorAll('.stream-items > .stream-item')).map((e) => {
+					const permalink = e.querySelector('.js-permalink');
+					const link = permalink === null ? null : permalink.getAttribute('href');
+					const images = Array.from(e.querySelectorAll('.AdaptiveMedia')).map((e) => {
+						return Array.from(e.querySelectorAll('img'));
+					}).reduce((a, b) => {
+						return a.concat(b);
+					}, []).map((e) => {
+						return `${e.getAttribute('src')}:orig`;
+					});
+
+					return {
+						'link': link,
+						'images': images,
+					};
+				});
+			});
 
 			try {
 				await page.focus('.has-more-items');
+
+				if(prevLength === tweets.length) {
+					if(count > 1) {
+						shouldScroll = false;
+					}
+					await sleep(15 * 1000);
+					++count;
+				}
 			}
 			catch(err) {
 				shouldScroll = false;
 			}
-			await page.evaluate((_) => {
-				window.scrollBy(0, document.documentElement!.scrollHeight);
-			});
 		}
 		while(shouldScroll);
 
-		let images = [];
-		do {
-			await sleep(500);
+		console.log(tweets);
 
-			images = await page.evaluate((_) => {
-				return Array.from(document.querySelectorAll('.AdaptiveMedia')).map((e) => {
-					return Array.from(e.querySelectorAll('img'));
-				}).reduce((a, b) => {
-					return a.concat(b);
-				}, []).map((e) => {
-					return `${e.getAttribute('src')}:orig`;
-				});
-			});
-		}
-		while(images.length === 0);
+		await sleep(500);
 
-		for(const image of images) {
-			console.log(`[${images.indexOf(image) + 1}/${images.length}] download url: ${image}`);
-			await download(name, image);
-		}
+		// const images = tweets.map((tweet) => {
+		// 	return tweet.images;
+		// }).reduce((a, b) => {
+		// 	return a.concat(b);
+		// }, []);
+		// const length = images.length;
+		// for(let i = 0; i < length; ++i) {
+		// 	const image = images[i];
+		// 	console.log(`[${i + 1}/${length}] download url: ${image}`);
+		// 	await download(name, image);
+		// }
 
 		await page.close();
 	}
